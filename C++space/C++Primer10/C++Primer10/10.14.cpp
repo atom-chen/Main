fb4877421d6722606dd76a3fd08ec3ebca676c78
WangBiyu <251237220@qq.com>
2018-02-05 11:45:51 +0800

    update

---
+++ C++space/C++Primer10/C++Primer10/10.14.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+void Sum()
+{
+	int ii[] = { 2, 35, 342, 3123, 22, 335, 552, 223, 44 };
+	
+	
+}
+
+int main14()
+{
+   [](const int& a, const int& b)->int{ return a + b; };
+   return 0;
+}
---
+++ C++space/C++Primer10/C++Primer10/10.15.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+int main15()
+{
+	int bdd = 9;
+	[bdd](const int& num){return bdd + num; };
+   return 0;
+}
