b7848987443c3fe4c221dcfd6e4a6387a8d0037e
Home2 <251237220@qq.com>
2018-03-26 23:33:17 +0800

    HOME2

--- InputStream/SpaceShooter/Assets/_Scripts/Tags.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Tags{
-    public static string Boundary = "Boundary";
-    public static string Player = "Player";
-    public static string Bolt = "Bolt";
-    public static string gameController = "GameController";
-}
