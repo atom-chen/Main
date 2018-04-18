a8e198ec78ef8f66d4864aabd5d0cba54af53eeb
Home2 <251237220@qq.com>
2018-04-18 21:39:01 +0800

    HOME2

---
+++ OpenGL/Shader/光源_加强/OpenGL渲染环境/ParticleSystem.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "RenderAble.h"
+
+#define DELAYTIME 0.1f
+
+
+class ParticleSystem:public RenderAble
+{
+public:
+	virtual void Init(const vec3& position, const int& maxNum = 180, const char* picPath = "res/test.bmp");
+public:
+	virtual void Update();
+	void Draw();
+	virtual void Destory();
+protected:
+	float m_StartLiftTime=15;//每个粒子存活时间
+	int m_MaxPar;//最大粒子数
+	float angle = 0.0f;
+};
