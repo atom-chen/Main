d70f6616b1ff3bb2848bb9abe113b82d43670ae3
Home2 <251237220@qq.com>
2018-04-19 23:43:58 +0800

    HOME2

---
+++ Lua/控制语句/Untitled1.lua
@@ -0,0 +1,21 @@
+a=true
+a=false
+b=false
+
+if a then
+print("a is true")
+end
+
+if a then
+print ("a is true")
+else
+print("a is false")
+end
+
+if a then
+print("a is true")
+elseif b then
+print("b is true")
+else
+print("a and b is false")
+end
---
+++ Lua/表达式/Untitled1.lua
@@ -0,0 +1,32 @@
+a=1
+b=2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a^b)
+print(-a)
+
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a==b)
+print(a~=b)
+
+a=true
+b=false
+print(a and b)
+print(a or b)
+print(not a)
+print(1 and true)
+print(-1 and true)
+print(nil and true)
+
+a="hello"
+b="world"
+print(a..b)
+
+
+a={}
+
