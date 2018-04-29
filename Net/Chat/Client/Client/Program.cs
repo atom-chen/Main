145c14cb9c36c59fd626f3a5779310d41a6581e0
Home2 <251237220@qq.com>
2018-04-29 15:33:44 +0800

    HOME2

---
+++ Net/Chat/Client/Client/Program.cs
@@ -0,0 +1,57 @@
+﻿using ExitGames.Client.Photon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+  class ChatListener:IPhotonPeerListener
+  {
+    public bool m_IsConnect = false;
+    //debug的回调
+    public void DebugReturn(DebugLevel level, string message)
+    {
+
+    }
+    //事件处理函数
+    public void OnEvent(EventData eventData)
+    {
+
+    }
+    //服务器响应
+    public void OnOperationResponse(OperationResponse operationResponse)
+    {
+
+    }
+    //当连接状态改变时
+    public void OnStatusChanged(StatusCode statusCode)
+    {
+      switch(statusCode)
+      {
+        case StatusCode.Connect:
+          Console.WriteLine("连接成功");
+          m_IsConnect = true;
+          break;
+        default:
+          Console.WriteLine("其他状态");
+          break;
+      }
+    }
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      ChatListener listener=new ChatListener();
+      PhotonPeer peer = new PhotonPeer(listener,ConnectionProtocol.Tcp);
+      peer.Connect("127.0.0.1:4530", "ChatServer");//添加请求到服务器
+      Console.WriteLine("正在连接...");
+      while(!listener.m_IsConnect)
+      {
+        peer.Service();//立即发包
+      }
+    }
+  }
+}
---
+++ Net/Chat/Server/Server/ChatPeer.cs
@@ -0,0 +1,28 @@
+﻿using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+  //与客户端进行通信
+  class ChatPeer : PeerBase
+  {
+    public ChatPeer(IRpcProtocol protocol, IPhotonPeer unmanagedPeer):base(protocol,unmanagedPeer)
+    {
+      
+    }
+    protected override void OnDisconnect(PhotonHostRuntimeInterfaces.DisconnectReason reasonCode, string reasonDetail)
+    {
+
+    }
+
+    protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+    {
+
+    }
+  }
+}
