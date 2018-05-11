87b3a5d1307626e7118e061be0f251946bbb0e5c
Home2 <251237220@qq.com>
2018-05-11 16:24:48 +0800

    home2

---
+++ Game/MMORPG/MMOMain/Assets/Script/UI/LaunchScene/CreateRoleMenu/SelectRoleItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectRoleItem : MonoBehaviour {
+  public byte m_Sex;
+
+  public void Init(byte sex)
+  {
+    m_Sex = sex;
+  }
+}
---
+++ Game/MMORPG/MMOMain/Assets/Script/UI/LaunchScene/StartManu/ServerChoose/ServerListLogic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerListLogic : MonoBehaviour {
+  public UIGrid m_Grid;
+  public GameObject m_HotServerPrefab;
+  public GameObject m_NormalServerPrefab;
+  public void InitServerList(List<ServerProperty> serverList)
+  {
+    List<Transform> childList=m_Grid.GetChildList();
+    for (int i = 0; i < childList.Count;i++)
+    {
+      DestroyImmediate(childList[i].gameObject);
+    }
+    foreach (ServerProperty item in serverList)
+    {
+      ServerItem serverItem;
+      if (item.Count >= 50)
+      {
+        serverItem = NGUITools.AddChild(m_Grid.gameObject, m_HotServerPrefab).GetComponent<ServerItem>();
+      }
+      else
+      {
+        serverItem = NGUITools.AddChild(m_Grid.gameObject, m_NormalServerPrefab).GetComponent<ServerItem>();
+      }
+      if (serverItem != null)
+      {
+        serverItem.InitItem(item);
+      }
+
+      }
+    m_Grid.Reposition();
+  }
+}
