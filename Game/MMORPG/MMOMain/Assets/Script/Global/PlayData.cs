aa0df737fabd626f6af500fc2bfd097942444809
Home2 <251237220@qq.com>
2018-05-17 23:57:00 +0800

    HOME2

--- Game/MMORPG/MMOMain/Assets/Script/Global/PlayData.cs
+++ Game/MMORPG/MMOMain/Assets/Script/Global/PlayData.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 
 
-class PlayData
+public class PlayData
 {
   public static User UserData;
   public static ServerProperty ServerData;
--- Game/MMORPG/MMOMain/Assets/Script/Net/Common/Model/Role.cs
+++ Game/MMORPG/MMOMain/Assets/Script/Net/Common/Model/Role.cs
@@ -6,9 +6,14 @@ using System.Text;
 
 public class Role
 {
-  public int ID;
-  public string Name;
-  public uint Level;
-  public bool Sex;
-  public int UserID;
+  public int ID;//ID
+  public string Name;//名称
+  public uint Level;//等级
+  public bool Sex;//性别
+  public int UserID;//所属用户
+  public string HeadIcon;//头像
+  public int Exp;//经验
+  public int Coin;//金币
+  public int Energy;//体力
+  public int Toughen;//历练数
 }
