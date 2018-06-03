71357ffd811d294f4f6d846c1a04e8032cbd709a
wangbiyu <251237220@qq.com>
2018-06-03 23:35:20 +0800

    home1

---
+++ Game/MMORPG/MMOServer/role.sql
@@ -0,0 +1,32 @@
+﻿# Host: 127.0.0.1  (Version: 5.6.23-log)
+# Date: 2018-06-03 23:30:05
+# Generator: MySQL-Front 5.3  (Build 4.205)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "role"
+#
+
+CREATE TABLE `role` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(8) NOT NULL DEFAULT '',
+  `Level` int(11) unsigned NOT NULL DEFAULT '1',
+  `Sex` bit(1) NOT NULL DEFAULT b'0',
+  `UserID` int(11) NOT NULL DEFAULT '0',
+  `HeadIcon` varchar(30) NOT NULL DEFAULT '3' COMMENT '头像spriteName',
+  `Exp` int(11) NOT NULL DEFAULT '0' COMMENT '经验',
+  `coin` int(11) NOT NULL DEFAULT '0' COMMENT '金币',
+  `YuanBao` int(11) NOT NULL DEFAULT '0' COMMENT '元宝',
+  `Energy` int(11) NOT NULL DEFAULT '0' COMMENT '体力',
+  `Toughen` int(11) NOT NULL DEFAULT '0' COMMENT '历练',
+  `LastDownLine` datetime DEFAULT '0000-00-00 00:00:00' COMMENT '上次下线时间',
+  PRIMARY KEY (`Id`),
+  KEY `外键` (`UserID`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "role"
+#
+
+INSERT INTO `role` VALUES (1,'里白吉',100,b'1',4,'3',0,0,0,160,160,'2018-05-30 09:54:17'),(2,'吉百利',100,b'0',4,'3',0,0,4,160,160,'2018-05-30 09:48:25'),(3,'Mlxg',1,b'0',6,'3',0,0,0,0,0,NULL),(4,'立白吉',1,b'1',8,'3',0,0,0,0,0,NULL),(5,'Ming',1,b'0',9,'3',0,0,0,0,0,NULL),(6,'Uzi的卡莎',1,b'0',6,'3',0,0,1,61,61,'2018-05-24 16:08:28');
---
+++ Game/MMORPG/MMOServer/serverpropert.sql
@@ -0,0 +1,23 @@
+﻿# Host: 127.0.0.1  (Version: 5.6.23-log)
+# Date: 2018-06-03 23:30:18
+# Generator: MySQL-Front 5.3  (Build 4.205)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "serverpropert"
+#
+
+CREATE TABLE `serverpropert` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `IP` varchar(16) NOT NULL DEFAULT '127.0.0.1',
+  `Name` varchar(30) NOT NULL DEFAULT '',
+  `Count` int(11) NOT NULL DEFAULT '20',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "serverpropert"
+#
+
+INSERT INTO `serverpropert` VALUES (1,'192.168.2.102','火云洞',20),(2,'192.168.2.102','盘丝洞',50),(3,'192.168.2.102','魔王寨',50),(4,'192.168.2.102','狮驼岭',50),(5,'192.168.2.102','大唐官府',2000),(6,'192.168.2.102','化生寺',20),(7,'192.168.2.102','女儿村',90),(8,'192.168.2.102','方寸山',10);
