ced53b9b3ea4c77f7e33157189d09a0222c4ee08
wangbiyu <251237220@qq.com>
2018-06-06 21:48:58 +0800

    home1

---
+++ Unity/TPS/Client/Assets/Scripts/MainCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour {
+    private Transform m_CameraTransform;
+
+    Transform m_TargetTransform;
+    float distance = 5.0f;//水平距离
+    float height = 5;//高度
+    float cameraSpeed = 20.0f;
+	void Start ()
+    {
+		m_CameraTransform=this.transform;
+        m_TargetTransform = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	void Update () 
+    {
+        Translate();
+        Rotate();
+
+        
+	}
+    void Translate()
+    {
+        Vector3 targerPos = m_TargetTransform.position - Vector3.forward * distance + Vector3.up * height;
+
+        m_CameraTransform.position = Vector3.Lerp(m_CameraTransform.position, targerPos, cameraSpeed * Time.deltaTime);
+    }
+
+    void Rotate()
+    {
+        //修改摄像机朝向
+        Quaternion targetRotation = Quaternion.LookRotation((m_TargetTransform.position - (m_CameraTransform.position+Vector3.down*3)).normalized);
+        transform.rotation = Quaternion.Slerp(targetRotation,m_CameraTransform.rotation, cameraSpeed * Time.deltaTime);
+        //m_CameraTransform.LookAt(m_TargetTransform.position);
+    }
+}
---
+++ Unity/TPS/Client/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private Transform m_PlayerTramsform;
+
+    float m_Vertical;
+    float m_Horizontal;
+    float m_MouseX;
+    float m_MoveSpeed=5.0f;
+    float m_RotateSpeed = 100.0f;
+    void Start()
+    {
+        m_PlayerTramsform = this.transform;
+    }
+    void Update()
+    {
+        m_Vertical=Input.GetAxis("Vertical");
+        m_Horizontal = Input.GetAxis("Horizontal");
+        m_MouseX=Input.GetAxis("Mouse X");
+        TransLate();
+        Rotate();
+    }
+
+
+    void TransLate()
+    {
+        if(m_Vertical!=0 || m_Horizontal!=0)
+        {
+            Vector3 moveDir = (Vector3.forward * m_Vertical + Vector3.right * m_Horizontal).normalized;
+            m_PlayerTramsform.Translate(moveDir * m_MoveSpeed * Time.deltaTime, Space.Self);
+        }
+    }
+
+    void Rotate()
+    {
+        if(m_MouseX!=0)
+        {
+            m_PlayerTramsform.Rotate(Time.deltaTime * m_RotateSpeed * Vector3.up*m_MouseX, Space.Self);
+        }
+    }
+}
