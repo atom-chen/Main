52c7f29d5db11dcf0c450772c68a2a49b4c99090
shenzhen <251237220@qq.com>
2018-06-28 21:41:24 +0800

    shenzhen

---
+++ Lua/LuaSpace/函数/MoreReturn.lua
@@ -0,0 +1,19 @@
+function f(a,b)
+	print("input",a,b)
+	return a or b
+end
+
+a=f(3);
+print(a);
+
+a=f(3,4);
+print(a);
+
+a=f(3,4,5);
+print(a);
+
+count=0;
+function incCount(a)
+	n=n or 1 --dafault parameter = 1,·ÀÖ¹Êµ²ÎÎªnil
+	count=count+n
+end
