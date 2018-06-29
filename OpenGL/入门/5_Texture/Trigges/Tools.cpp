470e2991d08bc3867190195aa7a771496b6c88ce
wangbiyu <251237220@qq.com>
2018-06-29 19:27:39 +0800

    home1

--- OpenGL/入门/5_Texture/Trigges/Tools.cpp
+++ OpenGL/入门/5_Texture/Trigges/Tools.cpp
@@ -32,3 +32,98 @@ bool LoadTGATexture(const char *szFileName, GLenum minFilter, GLenum magFilter, 
 
 	return true;
 }
+
+void MakePyramid(GLBatch& pyramidBatch)
+{
+	pyramidBatch.Begin(GL_TRIANGLES, 18, 1);
+
+	// Bottom of pyramid
+	pyramidBatch.Normal3f(0.0f, -1.0f, 0.0f);
+	pyramidBatch.MultiTexCoord2f(0, 0.0f, 0.0f);
+	pyramidBatch.Vertex3f(-1.0f, -1.0f, -1.0f);
+
+	pyramidBatch.Normal3f(0.0f, -1.0f, 0.0f);
+	pyramidBatch.MultiTexCoord2f(0, 1.0f, 0.0f);
+	pyramidBatch.Vertex3f(1.0f, -1.0f, -1.0f);
+
+	pyramidBatch.Normal3f(0.0f, -1.0f, 0.0f);
+	pyramidBatch.MultiTexCoord2f(0, 1.0f, 1.0f);
+	pyramidBatch.Vertex3f(1.0f, -1.0f, 1.0f);
+
+	pyramidBatch.Normal3f(0.0f, -1.0f, 0.0f);
+	pyramidBatch.MultiTexCoord2f(0, 0.0f, 1.0f);
+	pyramidBatch.Vertex3f(-1.0f, -1.0f, 1.0f);
+
+	pyramidBatch.Normal3f(0.0f, -1.0f, 0.0f);
+	pyramidBatch.MultiTexCoord2f(0, 0.0f, 0.0f);
+	pyramidBatch.Vertex3f(-1.0f, -1.0f, -1.0f);
+
+	pyramidBatch.Normal3f(0.0f, -1.0f, 0.0f);
+	pyramidBatch.MultiTexCoord2f(0, 1.0f, 1.0f);
+	pyramidBatch.Vertex3f(1.0f, -1.0f, 1.0f);
+
+
+	M3DVector3f vApex = { 0.0f, 1.0f, 0.0f };
+	M3DVector3f vFrontLeft = { -1.0f, -1.0f, 1.0f };
+	M3DVector3f vFrontRight = { 1.0f, -1.0f, 1.0f };
+	M3DVector3f vBackLeft = { -1.0f, -1.0f, -1.0f };
+	M3DVector3f vBackRight = { 1.0f, -1.0f, -1.0f };
+	M3DVector3f n;
+
+	// Front of Pyramid
+	m3dFindNormal(n, vApex, vFrontLeft, vFrontRight);
+	pyramidBatch.Normal3fv(n);
+	pyramidBatch.MultiTexCoord2f(0, 0.5f, 1.0f);
+	pyramidBatch.Vertex3fv(vApex);		// Apex
+
+	pyramidBatch.Normal3fv(n);
+	pyramidBatch.MultiTexCoord2f(0, 0.0f, 0.0f);
+	pyramidBatch.Vertex3fv(vFrontLeft);		// Front left corner
+
+	pyramidBatch.Normal3fv(n);
+	pyramidBatch.MultiTexCoord2f(0, 1.0f, 0.0f);
+	pyramidBatch.Vertex3fv(vFrontRight);		// Front right corner
+
+
+	m3dFindNormal(n, vApex, vBackLeft, vFrontLeft);
+	pyramidBatch.Normal3fv(n);
+	pyramidBatch.MultiTexCoord2f(0, 0.5f, 1.0f);
+	pyramidBatch.Vertex3fv(vApex);		// Apex
+
+	pyramidBatch.Normal3fv(n);
+	pyramidBatch.MultiTexCoord2f(0, 1.0f, 0.0f);
+	pyramidBatch.Vertex3fv(vBackLeft);		// Back left corner
+
+	pyramidBatch.Normal3fv(n);
+	pyramidBatch.MultiTexCoord2f(0, 0.0f, 0.0f);
+	pyramidBatch.Vertex3fv(vFrontLeft);		// Front left corner
+
+	m3dFindNormal(n, vApex, vFrontRight, vBackRight);
+	pyramidBatch.Normal3fv(n);
+	pyramidBatch.MultiTexCoord2f(0, 0.5f, 1.0f);
+	pyramidBatch.Vertex3fv(vApex);				// Apex
+
+	pyramidBatch.Normal3fv(n);
+	pyramidBatch.MultiTexCoord2f(0, 1.0f, 0.0f);
+	pyramidBatch.Vertex3fv(vFrontRight);		// Front right corner
+
+	pyramidBatch.Normal3fv(n);
+	pyramidBatch.MultiTexCoord2f(0, 0.0f, 0.0f);
+	pyramidBatch.Vertex3fv(vBackRight);			// Back right cornder
+
+
+	m3dFindNormal(n, vApex, vBackRight, vBackLeft);
+	pyramidBatch.Normal3fv(n);
+	pyramidBatch.MultiTexCoord2f(0, 0.5f, 1.0f);
+	pyramidBatch.Vertex3fv(vApex);		// Apex
+
+	pyramidBatch.Normal3fv(n);
+	pyramidBatch.MultiTexCoord2f(0, 0.0f, 0.0f);
+	pyramidBatch.Vertex3fv(vBackRight);		// Back right cornder
+
+	pyramidBatch.Normal3fv(n);
+	pyramidBatch.MultiTexCoord2f(0, 1.0f, 0.0f);
+	pyramidBatch.Vertex3fv(vBackLeft);		// Back left corner
+
+	pyramidBatch.End();
+}
--- OpenGL/入门/5_Texture/Trigges/Tools.h
+++ OpenGL/入门/5_Texture/Trigges/Tools.h
@@ -2,3 +2,5 @@
 #include "main.h"
 
 bool LoadTGATexture(const char* filePath, GLenum minFilter = GL_LINEAR, GLenum magFilter = GL_LINEAR, GLenum wrapMode = GL_CLAMP_TO_EDGE);
+
+void MakePyramid(GLBatch& pyramidBatch);
