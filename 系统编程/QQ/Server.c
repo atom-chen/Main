b330c798b0065a389505a51c9464a92655b1865d
linux <251237220@qq.com>
2018-07-03 23:37:37 +0800

    sssssss

--- 系统编程/QQ/Server.c
+++ 系统编程/QQ/Server.c
@@ -12,10 +12,42 @@ this is QQ Server
 
 #define SERVER_ADDR /home/uzi/桌面/gitSPace/系统编程/QQ/ServerFifo
 #define FILE_MODEL (S_IRUSR | S_IWUSR | S_IRGRP | S_IWOTH)
-void Server(int serverID);
+struct MSG
+{
+	int send;
+	int receive;//id=-1:to server
+	char data[1024];
+}
+void Server(struct MSG msg);
+
+
 int main(int argc, char const *argv[])
 {
-	
+	if(mkfifo(SERVER_ADDR,FILE_MODEL)==-1)
+	{
+		perror("server mkpipe1 error\n");
+		exit(-1);
+	}
+	int fd=open(SERVER_ADDR,O_RDWR);
+	if(fd>0)
+	{
+		int ret;
+        struct MSG msg;
+        memset(&msg,0,sizeof(MSG));
+		while(1)
+		{
+			ret=read(fd,&msg,sizeof(MSG));//轮询
+			//if has msg
+			if(ret!=0)
+			{
+				Server(msg);
+			}
+            memset(msg,0,sizeof(MSG));
+		}
+	}
 	return 0;
 }
+void Server(struct MSG msg)
+{
 
+}
