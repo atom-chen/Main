65a3156871e8555ee1e28acfa473f583f259a139
linux <251237220@qq.com>
2018-07-03 19:16:16 +0800

    client

---
+++ 系统编程/QQ/Client.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <fcntl.h>
+
+
+#define SERVER_ADDR /home/uzi/桌面/gitSPace/系统编程/QQ/ServerFifo
+#define FILE_MODEL (S_IRUSR | S_IWUSR | S_IRGRP | S_IWOTH)
+void Client(int serverID);
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
