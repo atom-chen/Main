12ab5e459514dfe72f60cffa10c9257fed5d70c1
wangbiyu <251237220@qq.com>
2018-07-11 19:32:47 +0800

    6.2 flatshader

---
+++ OpenGL/进阶/6_FlatShader/GLSL/main.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <GLTools.h>	// OpenGL toolkit
+#include <GLMatrixStack.h>
+#include <GLFrame.h>
+#include <GLFrustum.h>
+#include <GLGeometryTransform.h>
+#include <StopWatch.h>
+
+#include <math.h>
+#ifdef __APPLE__
+#include <glut/glut.h>
+#else
+#define FREEGLUT_STATIC
+#include <GL/glut.h>
+#endif
+
+#pragma  comment(lib,"freeglut_static.lib")
+#pragma  comment(lib,"gltools.lib")
