40296c511f1d00be59c60b3f4131f41eadd42e80
wangbiyu <251237220@qq.com>
2018-08-26 16:16:55 +0800

    MMO服务器 文件

--- Game/MMORPG/MMOServer/CSMain/CSMain/Connect/Hander/SERVER_CG_ADD_ROLE.cs
+++ Game/MMORPG/MMOServer/CSMain/CSMain/Connect/Hander/SERVER_CG_ADD_ROLE.cs
@@ -6,16 +6,6 @@ using System.Threading.Tasks;
 
 public partial class CG_ADD_ROLE_PAK:CG_PAK_BASE
 {
-    private Dictionary<byte, object> dic = new Dictionary<byte, object>();
-    public Role _Role
-    {
-        set
-        {
-            dic.Remove((byte)ParameterCode.Role);
-            dic.Add((byte)ParameterCode.Role, ParaTools.GetJson<Role>(value));
-        }
-        get { return ParaTools.GetParameter<Role>(dic, ParameterCode.Role); }
-    }
     public override void Handle(User user)
     {
         user.HandlePacket(this);
