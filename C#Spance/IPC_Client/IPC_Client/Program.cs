50ef63b4e15369e02c8502167eac73561b7f1f5f
shenzhen <251237220@qq.com>
2018-08-28 11:53:42 +0800

    C# IPC

---
+++ C#Spance/IPC_Client/IPC_Client/Program.cs
@@ -0,0 +1,55 @@
+﻿using MessageObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Ipc;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPC_Client
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    IpcChannel tcc = new IpcChannel();
+        //    ChannelServices.RegisterChannel(tcc, false);
+        //    WellKnownClientTypeEntry remotEntry = new WellKnownClientTypeEntry(typeof(test), "ipc://testchannel/test");
+        //    RemotingConfiguration.RegisterWellKnownClientType(remotEntry);
+
+        //    test st = new test();
+        //    Console.WriteLine("{0},{1}", st.count(), st.Add(1));
+        //    Console.ReadLine();
+        //}
+        static void Main(string[] args)
+        {
+            RemoteObject objRemoteObject = ConnectServer();
+            Send(objRemoteObject);
+        }
+        private static void Send(RemoteObject objRemoteObject)
+        {
+            while (true)
+            {
+                Console.WriteLine("please input message...");
+                string message = Console.ReadLine();
+                try
+                {
+                    objRemoteObject.SendMessage(message);
+                }
+                catch (System.Runtime.Remoting.RemotingException)
+                {
+                    Console.WriteLine("can not connect message server");
+                }
+            }
+        }
+        private static RemoteObject ConnectServer()
+        {
+            IpcClientChannel channel = new IpcClientChannel();
+            ChannelServices.RegisterChannel(channel, false);
+            RemoteObject objRemoteObject = (RemoteObject)Activator.GetObject(typeof(RemoteObject), "ipc://ServerChannel/RemoteObject");
+            return objRemoteObject;
+        }
+    }
+}
---
+++ C#Spance/IPC_Server/IPC_Client/Message.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class test : MarshalByRefObject
+{
+    private int iCount = 0;
+    public int count()
+    {
+        iCount++;
+        return iCount;
+    }
+
+    public int Add(int x)
+    {
+        iCount += x;
+        return iCount;
+    }
+}
+namespace MessageObject
+{
+    //MarshalByRefObject 允许在支持远程处理的应用程序中跨应用程序域边界访问对象。
+    public class RemoteObject : MarshalByRefObject
+    {
+        public static Queue<string> qMessage { get; set; } //使用消息队列储存消息
+
+        public string SendMessage(string message)
+        {
+            if (qMessage == null)
+            {
+                qMessage = new Queue<string>();
+            }
+            qMessage.Enqueue(message);
+
+            return message;
+        }
+    }
+}
