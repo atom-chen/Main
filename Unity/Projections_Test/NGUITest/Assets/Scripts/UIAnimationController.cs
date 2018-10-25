878f8fe2cf6c866efce9601d5c5b484673014b66
shenzhen <251237220@qq.com>
2018-10-25 16:40:50 +0800

    NGUI修改

---
+++ Unity/Projections_Test/NGUITest/Assets/Scripts/UIAnimationController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIAnimationController : MonoBehaviour
+{
+    public UIPlayAnimation mUIPA;
+    void Start()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        mUIPA.PlayForward();
+    }
+
+    public void OnFinish()
+    {
+        Debug.Log("Finish");
+    }
+}
